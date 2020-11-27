c16349a9964d79a033e253cd10de2d419fe7ff60
caevh <Harrycave1234@outlook.com>
2020-11-27 10:35:15 +0000

    Part 4. Adding find_all to search for all jobs and then filter by publish date

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ import requests
 html_text = requests.get('https://www.indeed.co.uk/jobs?q=python&sort=date').text # Asigns all html text
 
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('div', class_='jobsearch-SerpJobCard')
-company_name = job.find('span', class_='company').text # used to return just the comany name
-skills = job.find('div', class_='jobCardReqList')
-published_date = job.find('span', class_='date').text
+jobs = soup.find_all('div', class_='jobsearch-SerpJobCard')
+for job in jobs:
+    published_date = job.find('span', class_='date').text
+    if 'Just' in published_date: 
+        company_name = job.find('span', class_='company').text # used to return just the comany name
+        skills = job.find('div', class_='jobCardReqList')
+    
 
-if skills == None:
-    print(f"""Company Name: {company_name}
-Minimal requirements: None specified""")
-else:
-    print(f"""Company Name: {company_name} 
-Minimal requirements: {skills.text}""")
+        if skills == None:
+            print(f"""Company Name:{company_name}
 
-print(published_date)
+Minimal requirements: None specified
+        """)
+        else:
+            print(f"""Company Name: {company_name} 
+        Minimal requirements: {skills.text}""")
+
+        
+
+        print(' ')
 
     
